de91a99bb88e568d6d27ec185ae7c06747c59c9f
Sylvie HOAREAU <hoareausms@gmail.com>
2025-09-26 13:13:11 +0400

    feat: mise en place du background

---
+++ Assets/Scripts/Background/ParallaxBackground.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float parallaxFactor;
+
+    private Vector3 lastCameraPosition;
+
+    void Start()
+    {
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 delta = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(delta.x * parallaxFactor, delta.y * parallaxFactor, 0);
+        lastCameraPosition = cameraTransform.position;
+    }
+}
